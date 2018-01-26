c393d31b7a7f2a6e8ae14a549555ef2f0fd5031f
Marius Nicolae <marius.a.nicolae@outlook.com>
2018-01-26 06:11:00 +0200

    minor

--- src/com/company/DOMparser.java
+++ src/com/company/DOMparser.java
@@ -28,13 +28,8 @@ public class DOMparser {
 
             File xmlFile = new File("C:\\Users\\Mariu\\Desktop\\ManipulateXML\\data\\cust.xml");
 
-
-
-
-
             DocumentBuilder builder =
                     DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            
 //            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 //            factory.setIgnoringComments(true);
 //            factory.setIgnoringElementContentWhitespace(true);
@@ -43,51 +38,38 @@ public class DOMparser {
             Document doc = builder.parse(xmlFile);
             doc.getDocumentElement().normalize();
 
-            continutFisier+="Root element :" + doc.getDocumentElement().getNodeName();
+            continutFisier+="Root element:" + doc.getDocumentElement().getNodeName() + ">";
 
-            NodeList nList = doc.getElementsByTagName("student");
-            continutFisier+=("----------------------------");
 
-            for (int i = 0; i < nList.getLength(); i++) {
-                Node nNode = nList.item(i);
-                continutFisier+="\nCurrent Element :" + nNode.getNodeName();
+            NodeList list = doc.getElementsByTagName("student");
+            for (int i = 0; i < list.getLength(); i++) {
+                Node node = list.item(i);
+                continutFisier+="\nCurrent Element :" + node.getNodeName();
 
                 //checking if type is element
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element child = (Element) node;
                     continutFisier+=" - Student roll no : "
-                            + eElement.getAttribute("rollno") + "\n";
-
+                            + child.getAttribute("rollno") + "\n";
 
                     continutFisier+="FirstName: "
-                            + eElement
-                            .getElementsByTagName("firstname")
-                            .item(0)
+                            + child.getElementsByTagName("firstname").item(0)
                             .getTextContent();
-
 
                     continutFisier+=" LastName: "
-                            + eElement
-                            .getElementsByTagName("lastname")
-                            .item(0)
+                            + child.getElementsByTagName("lastname").item(0)
                             .getTextContent();
-
 
                     continutFisier+=" Nickname: "
-                            + eElement
-                            .getElementsByTagName("nickname")
-                            .item(0)
+                            + child.getElementsByTagName("nickname").item(0)
                             .getTextContent();
 
-
                     continutFisier+="Marks: "
-                            + eElement
-                            .getElementsByTagName("marks")
-                            .item(0)
+                            + child.getElementsByTagName("marks").item(0)
                             .getTextContent();
                 }
             }
-            continutFisier+="\nTotal number of nodes: " + nList.getLength();
+            continutFisier+="\nTotal number of nodes: " + list.getLength();
             fisier.print(continutFisier);
             fisier.close();
         } catch (Exception e) {
